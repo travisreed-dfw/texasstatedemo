29fc805d73860b8367774a4edff45cbd7a8042c3
Travis Reed <traviswreed2@gmail.com>
2023-08-01 20:04:27 -0500

    Added popper files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,13 @@
 //Comment
+let contactEmail1 = document.getElementById('contactEmail1');
+let contactEmail2 = document.getElementById('contactEmail2');
+
+contactEmail2.addEventListener('keyup', () => {
+    if (contactEmail1.value === contactEmail2.value) {
+        contactEmail2.classList.remove('is-invalid');
+        console.log('Matches!');
+    } else {
+        contactEmail2.classList.add('is-invalid');
+        console.log("Doesn't Match!");
+    }
+});
